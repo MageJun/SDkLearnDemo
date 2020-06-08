286f12240691aac601feb6c83880850ee1dc7165
Mj <15101060292@163.com>
2020-06-08 09:56:08 +0800

    test4

--- app/src/main/java/com/xcm/sdklearndemo/MainActivity.java
+++ app/src/main/java/com/xcm/sdklearndemo/MainActivity.java
@@ -34,7 +34,9 @@ public class MainActivity extends AppCompatActivity {
 
         //commit test2
 
-        //commit test3
+        //delete commit test3
+
+        //commit test4
 
         initView();
     }
